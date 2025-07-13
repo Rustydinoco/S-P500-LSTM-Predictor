19bf540bdd746374f258d1dc5639d815fddd8faa
Bilal Achmad Ramadhan <114817547+Rustydinoco@users.noreply.github.com>
2025-07-13 14:41:25 +0700

    Add files via upload

---
+++ S&P500.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import plotly.graph_objects as go
+import streamlit as st
+from pyngrok import ngrok
+import getpass
+import traceback
+import joblib
+from sklearn.preprocessing import MinMaxScaler
+import time
+
+
+class LSTMModel(nn.Module):
+  def __init__(self, input_size = 1, hidden_layer_size  = 100, output= 1):
+    super().__init__()
+    self.hidden_layer_size = hidden_layer_size
+    self.lstm = nn.LSTM(input_size, hidden_layer_size)
+    self.linear = nn.Linear(hidden_layer_size, output)
+
+  def forward(self, input_seq):
+    lstm_out, _ = self.lstm(input_seq)
+    prediction = self.linear(lstm_out[:, -1, :])
+    return prediction
+
+
+@st.cache_resource
+def load_data(ticker):
+  df = yf.download(ticker, start="2010-01-01")
+  df.dropna(inplace = True)
+
+  if isinstance(df.columns, pd.MultiIndex):
+    df.columns = df.columns.get_level_values(0)
+
+  df.index = pd.to_datetime(df.index)
+  return df
+
+@st.cache_resource
+def load_model_and_scaler():
+  try:
+      model = LSTMModel()
+      model.load_state_dict(torch.load('model_LSTM_S&P500.pth'))
+      model.eval()
+
+      scaler = joblib.load('MinMaxScaler.joblib')
+      return model, scaler
+
+  except Exception as e:
+      st.error(f"Error : {e}")
+      st.text(traceback.format_exc())
+      raise e
+
+
+st.title("S&P 500 Stock Prediction")
+
+with st.spinner("Memuat Data dan Model......"):
+  time.sleep(3)
+  data = load_data("^GSPC")
+  model, scaler = load_model_and_scaler()
+
+if model is None or scaler is None:
+  st.stop()
+
+if model is not None:
+  st.subheader("Close Price S&P 500")
+  fig = go.Figure()
+  fig.add_trace(go.Scatter(x= data.index, y= data["Close"], name= "S&P 500"))
+  fig.layout.update(title_text = "S&P 500 Price Movement", xaxis_rangeslider_visible = True)
+  st.plotly_chart(fig)
+
+
+
+  st.sidebar.header("Prediksi Harga")
+
+  if st.sidebar.button("Prediksi Harga Selanjutnya"):
+    look_back = 60
+    last_60_days = data["Close"].values[-look_back:]
+    last_60_days_scaled = scaler.transform(last_60_days.reshape(-1,1))
+    X_pred = torch.FloatTensor(last_60_days_scaled).unsqueeze(0)
+    with torch.no_grad():
+      prediction_scaled = model(X_pred)
+
+    prediction_actual = scaler.inverse_transform(prediction_scaled.numpy().reshape(-1,1))
+
+    last_date = data.index[-1]
+    next_date = last_date + pd.Timedelta(days= 1)
+
+    st.sidebar.metric(
+            label=f"Prediksi Harga Tutup untuk {next_date.strftime('%Y-%m-%d')}",
+            value=f"${prediction_actual[0][0]:,.2f}"
+    )
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x= data.index, y= data["Close"], name= "S&P 500"))        
+    fig.add_trace(go.Scatter(x= [next_date], y= [prediction_actual[0][0]], name= "Hasil Prerdiksi",mode ="markers", marker = dict(color = "red", size = 10,symbol ="star")))
+    fig.layout.update(title_text = "S&P 500 Price Movement", xaxis_rangeslider_visible = True)
+    st.plotly_chart(fig)
+
+                    
+st.sidebar.info("Disclaimer: Ini adalah proyek teknis, bukan saran finansial.")
